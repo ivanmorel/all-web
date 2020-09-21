278dd52f4e4d83fbe0ba94ef752583538ec6e5b8
Diego Vergara <diego06884@hotmail.com>
2020-09-21 16:15:36 -0500

    [NO-STORY] Initial setup Add non-theme related ground work

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'cowaymega.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', 'babel-preset-shopify/web'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties', '@babel/transform-runtime']
+          }
+        }
+      }
+    ]
+  }
+};
