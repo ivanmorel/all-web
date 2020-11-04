ce84f976d16a5929cf5444766c90e270404a1273
Diego Vergara <diego06884@hotmail.com>
2020-11-04 16:27:15 -0500

    [ALL-39] Homepage Mobile navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,45 @@
-import $ from 'jquery';
-import ModuleTest from './modules/ModuleTest';
-import SubmenuList from './modules/SubmenuList';
-import Collapsible from './modules/Collapsible';
-import ProductMobileNav from './modules/ProductMobileNav';
-import PagesNav from './modules/PagesNav';
-import PagesMobileNav from './modules/PagesMobileNav';
-import DonationStatementAjaxCart from './modules/DonationStatementAjaxCart';
-import DonationStatementAjaxCartProduct from './modules/DonationStatementAjaxCartProduct';
-import DonationStatementAjaxCartTotal from './modules/DonationStatementAjaxCartTotal';
-import DonationsBar from './modules/DonationsBar';
+import $ from "jquery";
+import ModuleTest from "./modules/ModuleTest";
+import SubmenuList from "./modules/SubmenuList";
+import Collapsible from "./modules/Collapsible";
+import ProductMobileNav from "./modules/ProductMobileNav";
+import PagesNav from "./modules/PagesNav";
+import PagesMobileNav from "./modules/PagesMobileNav";
+import DonationStatementAjaxCart from "./modules/DonationStatementAjaxCart";
+import DonationStatementAjaxCartProduct from "./modules/DonationStatementAjaxCartProduct";
+import DonationStatementAjaxCartTotal from "./modules/DonationStatementAjaxCartTotal";
+import DonationsBar from "./modules/DonationsBar";
+import MobileNavToggle from "./modules/MobileNavToggle";
 
-$(document).ready(function() {
-  $('.js-submenu-list').each((index, element) => {
+$(document).ready(function () {
+  $(".js-submenu-list").each((index, element) => {
     new SubmenuList(element);
   });
-  $('.js-collapse').each((index, element) => {
+  $(".js-collapse").each((index, element) => {
     new Collapsible(element);
   });
-  $('.js-product-mobile-nav').each((index, element) => {
+  $(".js-product-mobile-nav").each((index, element) => {
     new ProductMobileNav(element);
   });
-  $('.js-pages-nav').each((index, element) => {
+  $(".js-pages-nav").each((index, element) => {
     new PagesNav(element);
   });
-  $('.js-pages-mobile-nav').each((index, element) => {
+  $(".js-pages-mobile-nav").each((index, element) => {
     new PagesMobileNav(element);
   });
-  $('.js-donation-statement-ajaxcart').each((index, element) => {
+  $(".js-donation-statement-ajaxcart").each((index, element) => {
     new DonationStatementAjaxCart(element);
   });
-  $('.js-donation-statement-ajaxcart__product').each((index, element) => {
+  $(".js-donation-statement-ajaxcart__product").each((index, element) => {
     new DonationStatementAjaxCartProduct(element);
   });
-  $('.js-donation-statement-ajaxcart__total').each((index, element) => {
+  $(".js-donation-statement-ajaxcart__total").each((index, element) => {
     new DonationStatementAjaxCartTotal(element);
   });
-  $('.js-donations-bar').each((index, element) => {
+  $(".js-donations-bar").each((index, element) => {
     new DonationsBar(element);
   });
-})
+  $(".mobile-nav__toggle").each((index, element) => {
+    new MobileNavToggle(element);
+  });
+});
